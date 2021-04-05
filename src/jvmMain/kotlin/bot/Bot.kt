da20bdb848dba15f78204890fdf2ac0c461cfaa1
mkulak <kleshney@gmail.com>
2021-04-05 22:56:24 +0200

    fix bots

--- src/jvmMain/kotlin/bot/Bot.kt
+++ src/jvmMain/kotlin/bot/Bot.kt
@@ -1,3 +1,4 @@
+import common.ClientCommand
 import common.MoveCommand
 import common.XY
 import common.length
@@ -21,17 +22,17 @@ val json = Json { ignoreUnknownKeys = true }
 suspend fun main() {
     val vertx = Vertx.vertx()
 //    val scope = CoroutineScope(coroutineContext)
-    repeat(100) {
-        GlobalScope.launch {
-            startBot(vertx)
+    repeat(10) { room ->
+        repeat(room * 2) { bot ->
+            GlobalScope.launch { startBot(vertx, bot, room) }
         }
     }
     delay(Long.MAX_VALUE)
 }
 
-private suspend fun startBot(vertx: Vertx) {
+private suspend fun startBot(vertx: Vertx, bot: Int, room: Int) {
     val ws = awaitResult<WebSocket> { handler ->
-        vertx.createHttpClient().webSocket(8080, "localhost", "/") {
+        vertx.createHttpClient().webSocket(8080, "localhost", "/rooms/$room") {
 //            if (it.succeeded()) {
 //                it.result().textMessageHandler {
 //                    println(it)
@@ -40,20 +41,22 @@ private suspend fun startBot(vertx: Vertx) {
             handler.handle(it)
         }
     }
-    println("connected")
+    println("connected bot#$bot room#$room")
     var pos = XY(nextDouble(1000.0), nextDouble(500.0))
     var target = pos
-    val inc = 10.0
+    val inc = 125.0
     delay(nextLong(1000))
     while (true) {
         val diff = target - pos
-        if (diff.length() > 10) {
-            val dxy = diff.normalize() * inc
+        val diffLen = diff.length()
+        if (diffLen > 1) {
+            val dxy = diff.normalize() * inc.coerceAtMost(diffLen)
             pos += dxy
-            ws.writeTextMessage(json.encodeToString(MoveCommand(pos)))
+//            println("sending pos $pos")
+            ws.writeTextMessage(json.encodeToString(MoveCommand(pos) as ClientCommand))
         } else {
             target = XY(nextDouble(1000.0), nextDouble(500.0))
         }
-        delay(nextLong(1000))
+        delay(250)
     }
 }
