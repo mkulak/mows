9aef31cfd4232966bbf7793e9fd003674974d41e
mkulak <mykhailo@wonder.me>
2021-04-22 14:04:22 +0200

    make tick interval customizable via env var

--- src/jvmMain/kotlin/server/RoomTicker.kt
+++ src/jvmMain/kotlin/server/RoomTicker.kt
@@ -5,19 +5,19 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
-import registry
 import java.util.concurrent.TimeUnit
 import kotlin.system.measureTimeMillis
 
 class RoomTicker(val scope: CoroutineScope, val gameService: GameService, registry: MeterRegistry) {
     val timer = registry.timer("tickDuration")
+    val tickInterval = System.getenv("TICK_INTERVAL")?.toInt() ?: 200
 
     fun start() {
         scope.launch {
             while (isActive) {
                 val elapsed = measureTimeMillis { gameService.tick() }
                 timer.record(elapsed, TimeUnit.MILLISECONDS)
-                val sleepDuration = TICK_FREQUENCY - elapsed
+                val sleepDuration = tickInterval - elapsed
                 if (sleepDuration > 0) {
                     delay(sleepDuration)
                 }
